2068f4eab56d576b75d110ff67783860d9cf5be5
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 03:32:07 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,13 @@
+def display_board(board)
+  puts " ${board[0]} | "
+end
+
+def valid_move?(board, index)
+  # Returns true if index is within 0-8 & is currently unoccupied by x or o token
+  # May want to code a position_taken?(board, index) to make this simpler
+end
+
+def move(board, index, token = X)
+  #Default token can be x.
+  # Set correct index value of the position withtin the board equal to the token
+end
