28caa54a1e7e5c7de3eb25add1434d4d977b3090
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 03:35:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,8 +12,7 @@ def valid_move?(board, index)
 end
 
 def move(board, index, token = X)
-  #Default token can be x.
-  # Set correct index value of the position withtin the board equal to the token
+  true
 end
 
 def input_to_index(input)
