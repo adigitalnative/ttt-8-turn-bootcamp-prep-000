03242e5297085f94f8f20b5bc6d32501ba5b455c
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 03:49:04 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  raise valid_position?(index).inspect
+  true
   # Returns true if index is within 0-8 & is currently unoccupied by x or o token
   # May want to code a position_taken?(board, index) to make this simpler
 end
